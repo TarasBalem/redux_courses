accf2254cdec4585bbcb8bab1d89fff4f1ec0368
Taras Balem <balemtaras@gmail.com>
2021-03-24 17:46:13 +0200

    cleaned configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
 import reducer from "./reducer";
-import {createLogger} from "redux-logger";
-import generateId from "./middlewares/generateId";
 
-const logger = createLogger({
-  collapse: true,
-});
-
-const middleware = [...getDefaultMiddleware(), logger, generateId];
+const middleware = [...getDefaultMiddleware()];
 export default function () {
   return configureStore({reducer, middleware});
 }
