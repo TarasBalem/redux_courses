1af80d142a9da4bb8ffc6c38e1a6b6fba6ff3d48
Taras Balem <balemtaras@gmail.com>
2021-03-26 16:22:34 +0200

    ready forms

--- src/components/courses/ManageCoursesPage.jsx
+++ src/components/courses/ManageCoursesPage.jsx
@@ -1,6 +1,8 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import {Redirect, useParams} from "@reach/router";
 import {courseAdded} from "../../store/courses";
 import useCourses from "../../hooks/useCourses";
+import CourseForm from "./CourseForm";
 
 const newCourse = {id: null, title: "", authorId: "", category: ""};
 
@@ -9,15 +11,45 @@ const ManageCoursesPage = () => {
   const [errors, setErrors] = useState({});
   const {dispatch, courses, authors} = useCourses();
 
-  const handleChange = (e) => {};
+  const {slug} = useParams();
+
+  useEffect(() => {
+    const course =
+      slug !== "new" && courses.length
+        ? courses.find((c) => c.slug === slug)
+        : newCourse;
+    if (course) {
+      setCourse(course);
+    } else {
+      setCourse(newCourse);
+    }
+  }, [courses, slug]);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setCourse((prev) => ({
+      ...prev,
+      [name]: name === "authorId" ? parseInt(value) : value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(course);
   };
 
   return (
     <div className="container mt-5">
-      <h1>ManageCourse</h1>
+      <div className="row">
+        <CourseForm
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          course={course}
+          authors={authors}
+          errors={errors}
+          saving={false}
+        />
+      </div>
     </div>
   );
 };
